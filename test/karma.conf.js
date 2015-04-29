4cb06acca8e0379442bc1eb95a1fab6505028f47
Carlos Castillo <sir.ragnar@gmail.com>
2015-04-29 23:45:27 +0200

    Fixed configuration with karma jasmine jquery

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,8 +33,8 @@ module.exports = function(config) {
       'bower_components/angular-local-storage/dist/angular-local-storage.js',
       'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
       // endbower
+      'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js',
